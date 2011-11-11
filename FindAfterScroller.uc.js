f63ef0bb89570f00b9a75f1cc0bb4d8628bf3105
Griever <c_idea@livedoor.com>
2011-11-11 23:52:40 +0900

    Update FindAfterScroller.uc.js

--- FindAfterScroller.uc.js
+++ FindAfterScroller.uc.js
@@ -2,9 +2,12 @@
 // @name           FindAfterScroller.uc.js
 // @namespace      http://d.hatena.ne.jp/Griever
 // @description    ページ内検索をで適度にスクロールする
-// @compatibility  Firefox 3.6, 4.0b6
+// @compatibility  Firefox 3.6, 8.0
 // @include        main
-// @note           検索結果を 3.6 では真ん中に、4.0 では画面端にならないようにスクロールする
+// @license        MIT License
+// @version        0.0.8
+// @note           検索結果を 3.6 では真ん中に、8.0 では画面端にならないようにスクロールする
+// @note           Firefox 8.0で動くように変更。Firefox 4～7は知らん。
 // ==/UserScript==
 
 gFindBar._findAgain_org = gFindBar._findAgain;
@@ -17,18 +20,12 @@ if ('getBoundingClientRect' in document.createRange()) {
 if (gFindBar._currentWindow) {
 	var controller = gFindBar._getSelectionController(gFindBar._currentWindow);
 	var selection = controller.getSelection(controller.SELECTION_NORMAL);
-	var margin = gFindBar._currentWindow.innerHeight / 10 * 3;
+	var margin = gFindBar._currentWindow.innerHeight / 4;
 	try {
 		var rect = selection.getRangeAt(0).getBoundingClientRect();
-		if (rect.top < margin) {
-			selection.QueryInterface(Components.interfaces.nsISelection2)
-			         .scrollIntoView(Components.interfaces.nsISelectionController.SELECTION_ANCHOR_REGION,
-			                         true, 30, 50);
-		}
-		else if (rect.bottom > gFindBar._currentWindow.innerHeight - margin) {
-			selection.QueryInterface(Components.interfaces.nsISelection2)
-			         .scrollIntoView(Components.interfaces.nsISelectionController.SELECTION_ANCHOR_REGION,
-			                         true, 70, 50);
+		if (rect.top < margin || rect.bottom > gFindBar._currentWindow.innerHeight - margin) {
+			selection.QueryInterface(Components.interfaces.nsISelectionPrivate)
+			         .scrollIntoView(controller.SELECTION_ANCHOR_REGION, true, 50, 80);
 		}
 	} catch (e) {  }
 }
